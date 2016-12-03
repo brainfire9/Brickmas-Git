b39f791915fcbbed78923f193923eb46d78a0a61
Mario Ramirez <brainfire9@gmail.com>
2016-12-03 09:44:01 -0700

    Added tween-in effect for bricks, added at Brick Container level.  Includes Animator, clip, and script to launch animation after delay in delaySeconds

---
+++ Assets/BrickRowTween.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BrickRowTween : MonoBehaviour 
+{
+	public float delaySeconds = 0f;
+
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () 
+	{
+		animator = GetComponent<Animator> ();
+
+		StartCoroutine (TweenIn());
+	}
+
+	IEnumerator TweenIn()
+	{
+		yield return new WaitForSeconds (delaySeconds);
+		animator.SetTrigger ("tweenInTrigger");
+	}
+}
