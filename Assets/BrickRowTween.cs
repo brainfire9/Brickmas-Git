47f2a12ba1ddfe41601e3f6c20a38d27cd8af7ad
Mario Ramirez <brainfire9@gmail.com>
2016-12-03 11:48:54 -0700

    Added Level 5, replaced level select menu with one that supports 9 levels, tweaked BrickTween to now run off Time.deltaTime instead of yield waitforseconds

--- Assets/BrickRowTween.cs
+++ Assets/BrickRowTween.cs
@@ -6,13 +6,26 @@ public class BrickRowTween : MonoBehaviour
 	public float delaySeconds = 0f;
 
 	private Animator animator;
+	private float timePassed = 0f;
 
 	// Use this for initialization
 	void Start () 
 	{
 		animator = GetComponent<Animator> ();
 
-		StartCoroutine (TweenIn());
+		// Old way
+		//StartCoroutine (TweenIn());
+
+	}
+
+	void Update()
+	{
+		timePassed += Time.deltaTime;
+
+		if (timePassed >= delaySeconds) 
+		{
+			animator.SetTrigger ("tweenInTrigger");
+		}
 	}
 
 	IEnumerator TweenIn()
@@ -20,4 +33,5 @@ public class BrickRowTween : MonoBehaviour
 		yield return new WaitForSeconds (delaySeconds);
 		animator.SetTrigger ("tweenInTrigger");
 	}
+
 }
